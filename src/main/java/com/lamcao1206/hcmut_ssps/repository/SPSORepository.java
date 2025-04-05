6f1d1c2735601d535d3f85104a97e0560ecd4c2c
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2025-04-05 22:07:30 +0700

    create 2 repository for two entities

---
+++ src/main/java/com/lamcao1206/hcmut_ssps/repository/SPSORepository.java
@@ -0,0 +1,12 @@
+package com.lamcao1206.hcmut_ssps.repository;
+
+import com.lamcao1206.hcmut_ssps.entity.SPSO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SPSORepository extends JpaRepository<Long, SPSO> {
+    Optional<SPSO> findByEmail(String email);
+}
