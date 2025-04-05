38441feb462b55b11e21aaa71df31ba5359c4d43
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2025-04-05 21:30:23 +0700

    create customer entity

---
+++ src/main/java/com/lamcao1206/hcmut_ssps/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.lamcao1206.hcmut_ssps.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "page_balance")
+    private int pageBalance;
+    
+    @Column(name = "last_login")
+    private Date lastLogin;
+}
