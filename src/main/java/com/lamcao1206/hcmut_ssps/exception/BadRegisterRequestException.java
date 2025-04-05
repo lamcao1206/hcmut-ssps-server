977c7cb70b807c23eb9f4f5b5a512b17feae2581
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2025-04-06 00:25:25 +0700

    implement the global exception handler and the BadRegisterRequestException for duplicate register

---
+++ src/main/java/com/lamcao1206/hcmut_ssps/exception/BadRegisterRequestException.java
@@ -0,0 +1,7 @@
+package com.lamcao1206.hcmut_ssps.exception;
+
+public class BadRegisterRequestException extends RuntimeException {
+    public BadRegisterRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/lamcao1206/hcmut_ssps/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.lamcao1206.hcmut_ssps.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BadRegisterRequestException.class)
+    public ResponseEntity<Map<String, Object>> handleDuplicateEmail(BadRegisterRequestException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", HttpStatus.CONFLICT.value());
+        error.put("error", "Bad Registation Request");
+        error.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        error.put("error", "Internal Server Error");
+        error.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
